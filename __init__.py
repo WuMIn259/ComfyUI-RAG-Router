ec49bafb316a562de655e91df03269fe3e38089f
WuMIn_059 <86294197+WuMIn259@users.noreply.github.com>
2026-06-24 23:46:34 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,15 +7,18 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # 从你的业务代码文件中导入节点类
 from .rag_router_node import ZImage_RAG_Router
+from .llm_purifier_node import ZImage_Prompt_Purifier
 
 # 注册节点类映射
 NODE_CLASS_MAPPINGS = {
-    "ZImage_RAG_Router": ZImage_RAG_Router
+    "ZImage_RAG_Router": ZImage_RAG_Router,
+    "ZImage_Prompt_Purifier": ZImage_Prompt_Purifier
 }
 
 # 注册节点在 ComfyUI UI 面板上的显示名称
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "ZImage_RAG_Router": "Z-Image RAG Router 🧠"
+    "ZImage_RAG_Router": "Z-Image RAG Router 🧠",
+    "ZImage_Prompt_Purifier": "Z-Image LLM Prompt Purifier 🤖"
 }
 
 # 必须包含这个 __all__ 列表，ComfyUI 靠它来加载节点
